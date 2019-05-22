961c9203f20a39db4d1b8f57059a5d9868ce20f1
szymenn <ilikedzimi@gmail.com>
2019-05-22 19:16:57 +0200

    modify game.cpp

--- Nine_mens_morris/game.cpp
+++ Nine_mens_morris/game.cpp
@@ -8,7 +8,7 @@ void game::print_state()
 	printf("A ------- B ------- C\n|         |         |\n|  D ---- E ---- F  |\n|  |      |      |  |\n");
 	printf("|  |  G-- H --I  |  |\n|  |  |       |  |  |\nJ--K--L       M--N--O\n|  |  |       |  |  |\n");
 	printf("|  |  P-- Q --R  |  |\n|  |      |      |  |\n|  S ---- T ---- U  |\n|         |         |\n");
-	printf("V ------- W ------- X\n");
+	printf("V ------- W ------- X");
 }
 
 
